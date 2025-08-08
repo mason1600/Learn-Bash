00f719d8e4df70d0791c8c800cec27b062dbdfd3
Mason <masonseaborn@gmail.com>
2025-08-07 19:01:16 -0500

    Add files via upload

---
+++ loopexample.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+while true; do 
+ clear
+echo ' ``_  _``  '
+ echo ' <0><0> '
+ echo '       '
+ echo ' __/\__ '
+echo '   |   '
+
+
+ cat << EOF        
+                   d<---rain drops           
+  d        d         
+      o          d    
+  d  -X-    d
+......t......................,
+EOF
+
+ echo '  OMOUS ' 
+ sleep .3 
+ clear  
+ echo '``_  _``  '
+echo '  <-><-> '
+ echo '       '
+ echo ' __/\__ '
+echo '   |  '
+cat << EOF        
+                     <---rain drops           
+  d        d         d
+   d  o   d           
+     -X-    d     dd
+..d...t....................,.
+EOF
+ echo '  ANON'   
+ sleep .3
+clear  
+ echo '``_  _``  '
+ echo '       '
+echo '        '
+ echo ' __/\__ '
+echo '   |  '
+cat << EOF        
+                    d<---rain drops           
+  d        d         d
+   d   o  d       d    
+  d   -X-  d      d
+..d....t....d..............,..
+EOF
+ echo '  ANON'   
+ sleep .3
+clear
+echo ' ``_  _``  '
+ echo ' <0><0> '
+ echo '       '
+ echo ' __/\__ '
+echo '   |   '
+
+
+ cat << EOF        
+                   d<---rain drops           
+  d        d         
+      o          d    
+  d  -X-    d
+......t..................,...
+EOF
+
+ echo '  OMOUS ' 
+ sleep .3 
+ done
