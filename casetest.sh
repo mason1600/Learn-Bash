168f03fb8ce9df1cb8617c35f12f149051945e50
Mason <masonseaborn@gmail.com>
2025-08-08 17:45:01 -0500

    Update casetest.sh

--- casetest.sh
+++ casetest.sh
@@ -1,33 +1,65 @@
 #!/bin/bash
 
+# echo command prints words to screen
 echo "hi testing case time give me a variable like pie or bob or cheese"
+
+# sleep command
 sleep .7
+
+# echo command again
 echo "pie"
+
+# another sleep pause
 sleep .8
+
+# echo command again
 echo "bob"
+
+# pause using sleep
 sleep .9
+
+# prints cheese to screen
 echo "cheese"
 
+# reads a variable into memory called VAR given by user
 read VAR
 
+# a case statement using VAR variable given by user
 case $VAR in
-	pie)
+
+# a choice here called pie ended by')'
+   pie)
+  
+   # witch then does this command echo
 		echo "yes thats pie great"
+ 
+  # the double semis is used between to seperate choices
 	;;
-	bob)
+	
+    bob)
 		echo "yes thats boby b baby"
-	;;
-	cheese)
+	
+    ;;
+	
+ cheese)
 		echo "oh yaa thats smelly cheese"
 	;;
+ 
+ # the asterisk handles all non options given or empty choice 
 	*)
 		echo "$VAR is not a thingy i asked you to type"
 	;;
+
+ # ends a case statement 'esac'
 esac
+
+# echo command telling user info
 echo "hi lets do another pic red blue or green"
 
+# reads another variable
 read VAR2
 
+# another example case statement .etc....etc..
 case $VAR2 in 
     red)
         echo "yes red"
@@ -49,5 +81,7 @@ EOF
     *)
         echo "$VAR2 is not a selection"
     ;;
+# ends the case statement	
 esac
+# exit command closes
 exit
