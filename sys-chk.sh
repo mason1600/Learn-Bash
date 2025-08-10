dbf40fcca367bfdea4df0443271a7edf4760d27d
Mason <masonseaborn@gmail.com>
2025-08-10 17:13:54 -0500

    Update sys-chk.sh

--- sys-chk.sh
+++ sys-chk.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-#check sys
+# check-system
 
+# echo command and free command show memory info
 echo "mem:"
 free -h
+
+# echo command and df command show disk info
 echo "disk usage:"
 df -h
+
+# echo command and uptime command show system uptime info
 echo "uptime:"
 uptime
 
+# read command holds user till done by pressing enter
 read hold
 
+# exit command exits
 exit
