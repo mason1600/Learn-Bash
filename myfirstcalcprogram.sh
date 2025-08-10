a46236757a46bbab1cca0f479378dbc56d3411ac
Mason <masonseaborn@gmail.com>
2025-08-10 17:00:40 -0500

    Update myfirstcalcprogram.sh

--- myfirstcalcprogram.sh
+++ myfirstcalcprogram.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# echo command asks user to give a number
 echo 'hello this is my first simple program a simple addition calc plz enter first number to add'
 
+# read command gathers first number as a variable
 read FIRST_NUMBER
 
+# echo command asks for another number to be added together
 echo 'alright we are almost there now enter another number to add together'
 
+# read command gathers second number as a variable
 read SECOND_NUMBER
 
+# echo command echos here is your number and performs arithmatic exspansion on numbers for a total
 echo 'here is your number' $(($FIRST_NUMBER + $SECOND_NUMBER))
 
+# echo command tells user hitting enter closes app or ends
 echo 'hitting enter will close or end'
 
-read THIRD_NUMBER
+# read command holds user till enter press
+read HOLD
+
+# exit command to exit
+exit
