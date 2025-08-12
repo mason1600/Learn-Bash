c592bf7a8f2068e8d160d244a2e4b5dd72e86bb0
Mason <masonseaborn@gmail.com>
2025-08-12 16:12:57 -0500

    Update loopexample.sh

--- loopexample.sh
+++ loopexample.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Now using one of my favorite simple loops a 'while true'; do loop and 5 echo commands i make my best anon face lol
 while true; do 
+
+ # this command called clear will clear the screen every iteration of the loop
  clear
+
+# These are the 5 echo commands talked about in the first comment
 echo ' ``_  _``  '
  echo ' <0><0> '
  echo '       '
  echo ' __/\__ '
 echo '   |   '
 
-
+# The cat command followed by two less than symbols is used to print everything inbetween the two EOF words standing for 'end of file'
  cat << EOF        
                    d<---rain drops           
   d        d         
@@ -16,10 +21,10 @@ echo '   |   '
   d  -X-    d
 ......t......................,
 EOF
-
+# Echo command used again
  echo '  OMOUS ' 
- sleep .3 
- clear  
+ sleep .3        # The sleep command pauses for however long you want i used .3 of a second here for example
+ clear                    # The clear command again
  echo '``_  _``  '
 echo '  <-><-> '
  echo '       '
@@ -67,4 +72,6 @@ EOF
 
  echo '  OMOUS ' 
  sleep .3 
+ 
+ # Do Not Forget To Add DONE at the end of your loop or prepare for head scratching and confusion
  done
