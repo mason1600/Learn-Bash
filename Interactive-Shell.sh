380dc0bbd74d1c73ff289e1fde9005170994214d
Mason <masonseaborn@gmail.com>
2025-09-04 09:00:57 -0500

    Update Interactive-Shell.sh

--- Interactive-Shell.sh
+++ Interactive-Shell.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# echo command asks user for first name
+# Echo command asks user for first name.
 echo what is your name first?
 
-# read command used to gather first name
+# Read command used to gather first name.
 read FIRST_NAME
 
-# another echo command used to ask user last name
+# Another echo command used to ask user last name.
 echo what is your last name?
 
-# another read command used to gather last name
+# Another read command used to gather last name.
 read LAST_NAME
 
-# echo command then says 'hello' with first and last name
+# Echo command then says 'hello' with first and last name.
 echo hello $FIRST_NAME $LAST_NAME
 
-# read command used to hold user till done pressing enter to move one
+# Read command used to hold user till done pressing enter to move one.
 read hold
 
-# exit command to exit
+# Exit command to exit.
 exit
