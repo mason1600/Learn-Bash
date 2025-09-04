92f76488f484c3e98fb09f4d699249edc409de2f
Mason <masonseaborn@gmail.com>
2025-09-04 09:04:49 -0500

    Update System-Check.sh

--- System-Check.sh
+++ System-Check.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# check-system
+# Check-system.
 
-# echo command and free command show memory info
+# Echo command and free command show memory info.
 echo "mem:"
 free -h
 
-# echo command and df command show disk info
+# Echo command and df command show disk info.
 echo "disk usage:"
 df -h
 
-# echo command and uptime command show system uptime info
+# Echo command and uptime command show system uptime info.
 echo "uptime:"
 uptime
 
-# read command holds user till done by pressing enter
+# Read command holds user till done by pressing enter.
 read hold
 
-# exit command exits
+# Exit command exits.
 exit
