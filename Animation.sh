aa8565fdf84751965620c816d35280305e767167
Mason <masonseaborn@gmail.com>
2025-09-04 08:34:33 -0500

    Update Animation.sh

--- Animation.sh
+++ Animation.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# function named animation is created to be called when ever animation is called
+# Function named animation is created to be called when ever animation is called.
 animation(){
-# cat command followed by two lessthan symbols will print what ever is inbetween the two EOFs stands for end of file
+# Cat command followed by two less-than symbols will print what ever is in-between the two EOFs stands for end of file.
 	cat << EOF
 
    o
   -X-
 ...t....................
 EOF
-# sleep for 1 second to get a slight pause between frames
+# Sleep for 1 second to get a slight pause between frames.
 sleep 1
         cat << EOF
 
@@ -93,7 +93,7 @@ sleep 1
 EOF
 sleep 1
 }
-# i call the animation 3 times to make sure it goes for a long time since this is a example
+# I call the animation 3 times to make sure it goes for a long time since this is a example.
 animation
 animation
 animation
@@ -102,7 +102,7 @@ animation
 read -t .1 -n 1 dummytrap
 read -t .1 -n 1 dummytrap
 read -t .1 -n 1 dummytrap
-# just a read command to hold the user till enter is pressed
+# Just a read command to hold the user till enter is pressed.
 read ani
-# exit command used at end to properly exit
+# Exit command used at end to properly exit.
 exit
