9962ba79838667aa2d018cd9117aaf7d20cca57f
Mason <masonseaborn@gmail.com>
2025-09-04 08:44:54 -0500

    Update Function-Test.sh

--- Function-Test.sh
+++ Function-Test.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# this is a echo command to prompt user to type cool
+# This is a echo command to prompt user to type cool.
 echo "function time plz type cool to call a function"
 
-# this creates function 'cool'
+# This creates function 'cool'.
 cool(){ 
 echo "you a cat"
 }
 
 
-# reads a variable
+# Reads a variable.
 read VAR
 
-# now this is a handy little peice of code here that checks if the variables file descripter is equal to that of a function string
+# Now this is a handy little peice of code here that checks if the variables file descripter is equal to that of a function string.
 if [[ $(type -t $VAR) == function ]]; then
     # and if so then Function exists, call it
     cool
 else
-    # Function does not exist, handle the situation (e.g., print an error)
+    # Function does not exist, handle the situation (e.g., print an error).
     echo "Error: $VAR does not exist."
 fi
 
-# another read command to simply hold user till enter press
+# Another read command to simply hold user till enter press.
 read hold
 
-# exit code to exit
+# Exit code to exit.
 exit
