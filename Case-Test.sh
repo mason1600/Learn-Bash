a6288fd5fcf059fdb300ccaf7509ea5cdd7117c6
Mason <masonseaborn@gmail.com>
2025-09-04 08:40:20 -0500

    Update Case-Test.sh

--- Case-Test.sh
+++ Case-Test.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-# echo command prints words to screen
+# Echo command prints words to screen.
 echo "hi testing case time give me a variable like pie or bob or cheese"
 
-# sleep command
+# Sleep command.
 sleep .7
 
-# echo command again
+# Echo command again.
 echo "pie"
 
-# another sleep pause
+# Another sleep pause.
 sleep .8
 
-# echo command again
+# Echo command again.
 echo "bob"
 
-# pause using sleep
+# Pause using sleep.
 sleep .9
 
-# prints cheese to screen
+# Prints cheese to screen.
 echo "cheese"
 
-# reads a variable into memory called VAR given by user
+# Reads a variable into memory called VAR given by user.
 read VAR
 
-# a case statement using VAR variable given by user
+# A case statement using VAR variable given by user.
 case $VAR in
 
-# a choice here called pie ended by')'
+# A choice here called pie ended by')'.
    pie)
   
-   # witch then does this command echo
+   # Witch then does this command echo.
 		echo "yes thats pie great"
  
-  # the double semis is used between to seperate choices
+  # The double semis is used between to seperate choices.
 	;;
 	
     bob)
@@ -45,21 +45,21 @@ case $VAR in
 		echo "oh yaa thats smelly cheese"
 	;;
  
- # the asterisk handles all non options given or empty choice 
+ # The asterisk handles all non options given or empty choice. 
 	*)
 		echo "$VAR is not a thingy i asked you to type"
 	;;
 
- # ends a case statement 'esac'
+ # Ends a case statement 'esac'.
 esac
 
-# echo command telling user info
+# Echo command telling user info.
 echo "hi lets do another pic red blue or green"
 
-# reads another variable
+# Reads another variable.
 read VAR2
 
-# another example case statement .etc....etc..
+# Another example case statement .etc....etc..
 case $VAR2 in 
     red)
         echo "yes red"
@@ -81,11 +81,11 @@ EOF
     *)
         echo "$VAR2 is not a selection"
     ;;
-# ends the case statement	
+# Ends the case statement.
 esac
 
-# just to hold user till enter press
+# Just to hold user till enter press.
 read hold
 
-# exit command closes
+# Exit command closes.
 exit
